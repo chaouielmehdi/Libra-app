973ab08f2ba74e5166f8244a41c1f2aae85dbc43
GitHubMehdii <mehdi.mc60@gmail>
2019-07-25 17:30:36 +0100

    Trying to test deployment

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -10,7 +10,7 @@ import * as anime from 'animejs';
 export class HomePage implements OnInit{
 	
 	isHeaderHidden: boolean = false;
-	areCardsHidden: boolean = false;
+	// areCardsHidden: boolean = false;
 	isFirstOpen: boolean = true;
 
   	constructor(
@@ -30,7 +30,7 @@ export class HomePage implements OnInit{
 		console.log('first open');
 
 		// hide cards
-		this.areCardsHidden = true;
+		// this.areCardsHidden = true;
 		
 		setTimeout(() => {
 			anime({
@@ -70,35 +70,35 @@ export class HomePage implements OnInit{
 
 	showCards(){
 		// animate cards
-		setTimeout(() => {
-			// make cards visible
-			this.areCardsHidden = false;
-		}, 50);
+		// setTimeout(() => {
+		// 	// make cards visible
+		// 	this.areCardsHidden = false;
+		// }, 50);
 
 		// animate cards
-		setTimeout(() => {
-			anime({
-				targets: '.right-card',
-				translateX: [5000, 0],
-				delay: function(el, i, l) {
-				  return i * 50;
-				},
-				duration: 1000,
-				easing: 'easeOutCirc'
-			});
-		}, 0);
+		// setTimeout(() => {
+		// 	anime({
+		// 		targets: '.right-card',
+		// 		translateX: [5000, 0],
+		// 		delay: function(el, i, l) {
+		// 		  return i * 50;
+		// 		},
+		// 		duration: 1000,
+		// 		easing: 'easeOutCirc'
+		// 	});
+		// }, 0);
 
-		// animate cards
-		setTimeout(() => {
-			anime({
-				targets: '.left-card',
-				translateX: [-5000, 0],
-				delay: function(el, i, l) {
-				  return i * 50;
-				},
-				duration: 1000,
-				easing: 'easeOutCirc'
-			});
-		}, 0);
+		// // animate cards
+		// setTimeout(() => {
+		// 	anime({
+		// 		targets: '.left-card',
+		// 		translateX: [-5000, 0],
+		// 		delay: function(el, i, l) {
+		// 		  return i * 50;
+		// 		},
+		// 		duration: 1000,
+		// 		easing: 'easeOutCirc'
+		// 	});
+		// }, 0);
 	}
 }
